41c8d9974f825fac63321a0478b0ee8d03aa4dbd
yagocrispim0 <yagocrispimgomes@gmail.com>
2022-01-19 11:55:14 -0300

    Classes ready

---
+++ CompositionEx2/Entities/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositionEx2.Entities
+{
+    internal class Comment
+    {
+        public string text { get; set; }
+
+        public Comment()
+        {
+
+        }
+        public Comment(string text)
+        {
+            this.text = text;
+        }
+    }
+}
---
+++ CompositionEx2/Entities/Post.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositionEx2.Entities
+{
+    internal class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+
+
+        public Post()
+        {
+
+        }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+    }
+}
